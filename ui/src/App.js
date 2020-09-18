ebefd9aa8c1eaae01d5d5082340a350869f2bec4
Marc Sturzl <msturzl@ford.com>
2020-09-18 18:28:21 -0400

    Create skeleton of UX without API integration

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,26 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { JoinForm } from "./JoinForm.js";
+import PhraseForm from "./PhraseForm.js";
+import ImageForm from "./ImageForm.js";
+import Results from "./Results.js";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [username, setUsername] = useState("");
+  const [apiStatus, setApiStatus] = useState("");
 
+  function setStateToInitialPhrase() {
+    // setApiStatus(axios.get('http://localhost:5000/status?username=' + username));
+  }
+
+  function getFakeResults() {
+    return [
+      {
+        username: "Marc",
+        phrases: ["Bacteria", "Alien invasion!"],
+        images: [
+          "https://www.samanthasbell.com/wp-content/uploads/2015/02/lukeydrawing.jpg",
+        ],
+      },
+      {
+        username: "Alice",
+        phrases: ["Punk rock", "Ed Edd n Eddy"],
+        images: [
+          "https://i.dailymail.co.uk/i/pix/2014/08/18/1408401439429_wps_44_King_s_College_London_new.jpg",
+        ],
+      },
+      {
+        username: "Bob",
+        phrases: ["Fun in a box", "Gameboy"],
+        images: [
+          "http://unrealitymag.com/wp-content/uploads/2009/04/kids_nintendo_6.jpg",
+        ],
+      },
+    ];
+  }
+
+  function usernameJoined(joinedUsername) {
+    setUsername(joinedUsername);
+    setApiStatus("SUBMIT_INITIAL_PHRASE");
+    // poll api
+  }
+
+  function phraseSubmitted() {
+    setApiStatus("SUBMIT_IMAGE");
+  }
+
+  function lastPhraseSubmitted() {
+    endGame();
+  }
+
+  function imageSubmitted() {
+    setApiStatus("SUBMIT_PHRASE");
+  }
+
+  function endGame() {
+    setApiStatus("GAME_OVER");
+  }
+
+  if (username === "") {
+    return (
+      <div className="App">
+        <JoinForm usernameJoined={usernameJoined} />
+      </div>
+    );
+  } else {
+    switch (apiStatus) {
+      case "SUBMIT_INITIAL_PHRASE":
+        return (
+          <div className="App">
+            <PhraseForm phraseSubmitted={phraseSubmitted} />
+          </div>
+        );
+      case "SUBMIT_PHRASE":
+        return (
+          <div className="App">
+            <PhraseForm
+              phraseSubmitted={lastPhraseSubmitted}
+              image={"an image"}
+            />
+          </div>
+        );
+      case "SUBMIT_IMAGE":
+        return (
+          <div className="App">
+            <ImageForm imageSubmitted={imageSubmitted} />
+          </div>
+        );
+      case "GAME_OVER":
+        return (
+          <div className="App">
+            <Results results={getFakeResults()} />
+          </div>
+        );
+      case "":
+      default:
+        return <div>Waiting for API...</div>;
+    }
+  }
+}
 export default App;
--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -1,9 +1,59 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, act, fireEvent, waitFor} from "@testing-library/react";
+import App from "./App";
+import axios from "axios";
+import '@testing-library/jest-dom/extend-expect'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("axios");
+
+//test('renders learn react link', () => {
+//  const { getByText } = render(<App />);
+//  const linkElement = getByText(/learn react/i);
+//  expect(linkElement).toBeInTheDocument();
+//});
+
+test("asks you to join the game with a username", () => {
+  const { getByText, getByLabelText } = render(<App />);
+  const headerText = getByText(/Join a Game/);
+  expect(headerText).toBeInTheDocument();
+  const usernameInputElement = getByLabelText(/Your Name/i);
+  expect(usernameInputElement).toBeInTheDocument();
+});
+
+test("hits the join API endpoint when you submit the join form", () => {
+  axios.post = jest.fn();
+  const { getByText, getByLabelText } = render(<App />);
+  const usernameInput = getByLabelText(/Your Name/);
+  expect(usernameInput).toBeInTheDocument();
+  act(() => {
+    fireEvent.change(usernameInput, { target: { value: "Billy" } });
+  });
+  const submitButton = getByText("JOIN");
+  expect(submitButton).toBeInTheDocument();
+  act(() => {
+    fireEvent.click(submitButton);
+  });
+  expect(axios.post).toHaveBeenCalledWith("http://localhost:5000/join", {
+    username: "Billy",
+  });
+});
+
+test("shows the submit phrase form when the API is in SUBMIT_INITIAL_PHRASE state", async () => {
+  axios.get = jest.fn(() => {
+    return {"description":"SUBMIT_INITIAL_PHRASE"};
+    
+  });
+  axios.post = jest.fn();
+  const { getByText, getByLabelText } = render(<App />);
+  const usernameInput = getByLabelText(/Your Name/);
+  expect(usernameInput).toBeInTheDocument();
+  act(() => {
+    fireEvent.change(usernameInput, { target: { value: "Billy" } });
+  });
+  const submitButton = getByText("JOIN");
+  expect(submitButton).toBeInTheDocument();
+  act(() => {
+    fireEvent.click(submitButton);
+  });
+  await waitFor(() => {expect(getByText('Submit a Phrase')).toBeInTheDocument()});
 });
