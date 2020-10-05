29cc330fcfdfe1e322bd9e2dabcca5462e6e4107
Connor Shaughnessy <sapphon@gmail.com>
2020-10-05 18:24:10 -0400

    Style results rudimentarily in flexbox [https://trello.com/c/9Ir374p7]

--- ui/src/helpercomponents/UserResultsSet.js
+++ ui/src/helpercomponents/UserResultsSet.js
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 
 export default function UserResultsSet(props) {
 
-return (<div>
-        <div className="originator-username">{props.username}</div>
+    function imageOrPhrase(index, submission) {
+        return index % 2 === 0 ? <div className="row-item">" {submission} "</div> : <img className="row-item" src={submission} alt=""/>;
+    }
+
+    function imageOrPhrasePlusLeaderText(index, submission) {
+        return index % 2 === 0 ? <><div className="row-item">" {submission} "</div><div className="row-item">Leading to...</div></> : <><img className="row-item" src={submission} alt=""/><div className="row-item">Leading to...</div></>;
+    }
+
+    return (<div className={"user-results-container"}>
+        <div className="row-item"> Player {props.username} originally submitted </div>
         {props.submissions.map((submission, index) => {
-            return index % 2 === 0 ? <div>{submission}</div> : <img src={submission} alt=""></img>
+            return index >= props.submissions.length - 1 ? imageOrPhrase(index, submission) : imageOrPhrasePlusLeaderText(index, submission);
         })}
     </div>);
 }
--- ui/src/statecomponents/Results.js
+++ ui/src/statecomponents/Results.js
@@ -10,19 +10,20 @@ export default function Results(props) {
   useEffect(() => {axios.get("http://localhost:5000/results")
   .then((response) => {setResults(response.data)})}, []);
 
-  return (
+    function getUserResultsSet(result) {
+        return <UserResultsSet
+            username={result.originator}
+            submissions={result.submissions}
+        />;
+    }
+
+    return (
     <div className="flex-column-container">
       <div className="header">
         <h1>Game Over!</h1>
         <div>(Are you winning, son?)</div>
       </div>
-      <div className="flex-row-container">
-        {results.map((result) => {return (<UserResultsSet
-        className="result-set"
-        username={result.originator}
-        submissions={result.submissions}
-      />)})}
-    </div>
+        {results.map((result) => {return getUserResultsSet(result)})}
     </div>
   );
 }
