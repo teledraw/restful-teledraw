cd9349fe718ce3963579c965562364d27c4dd04f
Connor Shaughnessy <sapphon@gmail.com>
2020-10-22 02:29:01 -0400

    Add user identity display to submit initial phrase screen [https://trello.com/c/adRGFjBG]

---
+++ ui/src/helpercomponents/IdentityPanel.js
@@ -0,0 +1,8 @@
+import React  from "react";
+
+export default function IdentityPanel({username, gamecode}) {
+    return (<div className={"identity"}>
+        <div>Your Name: {username}</div>
+        <div>This Room's Code: {gamecode}</div>
+    </div>)
+}
