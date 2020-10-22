a299a5a21e98cc9c278331f239606ced1d763b94
Connor Shaughnessy <sapphon@gmail.com>
2020-10-22 01:42:43 -0400

    Add API contract tests for image and phrase forms [https://trello.com/c/DoJfqAB9]

--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -23,6 +23,12 @@ function joinGame() {
     return rendered;
 }
 
+function submitPhrase(rendered) {
+    const {getByText, getByLabelText} = rendered;
+
+    return rendered;
+}
+
 
 describe('shows the right stuff to the user', () => {
     test("asks you to join the game with a username and game code", () => {
@@ -80,7 +86,7 @@ describe('shows the right stuff to the user', () => {
     });
 });
 describe('sends the right stuff to the API', () => {
-    test("hits the join API endpoint when you submit the join form", async () => {
+    test("hits the join API endpoint when you submit the join form", async (done) => {
         axios.post = jest.fn();
         axios.get = jest.fn(() => {
             return {data: {description: "WAIT"}};
@@ -90,7 +96,70 @@ describe('sends the right stuff to the API', () => {
             expect(axios.post).toHaveBeenCalledWith("http://localhost:5000/join", {
                 username: "Billy",
                 game: "TheClubhouse"
-            })
+            });
+            done();
+        });
+    });
+
+    test("hits the phrase API endpoint when you submit the phrase form", async (done) => {
+        axios.post = jest.fn();
+        axios.get = jest.fn(() => {
+            return {data: {description: "SUBMIT_INITIAL_PHRASE"}};
+        });
+        let {getByLabelText, getByText} = joinGame();
+        await wait(() => {
+            const phraseInput = getByLabelText(/Your common phrase, saying, or word/);
+            act(() => {
+                fireEvent.change(phraseInput, {target: {value: "No ghouls allowed"}});
+            });
+            const submitButton = getByText("SUBMIT");
+            act(() => {
+                fireEvent.click(submitButton);
+            });
+        });
+        await wait(() => {
+            expect(axios.post).toHaveBeenCalledWith("http://localhost:5000/phrase", {
+                username: "Billy",
+                game: "TheClubhouse",
+                phrase: "No ghouls allowed"
+            });
+            done();
+        });
+    });
+
+    test("hits the image API endpoint when you submit the image form", async (done) => {
+        axios.post = jest.fn();
+        axios.get = jest.fn(() => {
+            return {data: {description: "SUBMIT_IMAGE"}};
+        });
+
+        const imageFile = new File(['imageXXXimageYYYimageZZZ'], 'teledraw.png', {
+            type: 'image/png',
+        });
+
+        let {getByLabelText, getByText} = joinGame();
+        await wait(() => {
+            const imageInput = getByLabelText(/Your Drawing/);
+            act(() => {
+                Object.defineProperty(imageInput, 'files', {
+                    value: [imageFile],
+                    configurable: true
+                });
+                fireEvent.change(imageInput);
+            });
+            const submitButton = getByText("UPLOAD");
+            act(() => {
+                fireEvent.click(submitButton);
+            });
+        });
+        await wait(() => {
+            expect(axios.post).toHaveBeenCalledWith('http://localhost:5000/image',
+                {
+                    username: 'Billy',
+                    image: 'data:image/png;base64,aW1hZ2VYWFhpbWFnZVlZWWltYWdlWlpa',
+                    game: 'TheClubhouse'
+                });
+            done();
         });
     });
 });
