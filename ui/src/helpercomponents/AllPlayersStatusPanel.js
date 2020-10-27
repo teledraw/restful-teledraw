6c1b5f95bbc19a3f5ecd930f91c7015201987c57
Connor Shaughnessy <sapphon@gmail.com>
2020-10-26 21:06:19 -0400

    Show game status with more context for player info [https://trello.com/c/cZHPigtI]

--- ui/src/helpercomponents/AllPlayersStatusPanel.js
+++ ui/src/helpercomponents/AllPlayersStatusPanel.js
@@ -4,7 +4,7 @@ import {useInterval} from "../hooks/interval";
 import axios from 'axios';
 
 export default function AllPlayerStatusPanel({url, gamecode, username}) {
-    const [summary, setSummary] = useState([]);
+    const [summary, setSummary] = useState({players:[], canJoin:undefined});
 
     async function getSummary(url, gamecode){
         const summaryResponse = await axios.get(url+'?game='+gamecode);
@@ -23,11 +23,26 @@ export default function AllPlayerStatusPanel({url, gamecode, username}) {
         }
     }
 
+    function transformJoinabilityToHumanReadablePhrase(joinability){
+        switch (joinability) {
+            case undefined:
+                return "???";
+            case true:
+                return "Waiting for Players";
+            case false:
+                return "In Progress";
+        }
+    }
+
     useInterval(() => {getSummary(url, gamecode)}, 2000);
 
     return <div className={"all-players-status"}>
-        {summary.map(player => {
-            return <div className={"other-player-status"} key={player.username}>{player.username + (player.username === username ? " (YOU): " : ": ") + transformStatusToHumanReadableWord(player.status.description)}</div>
+        <div>Game Status: {transformJoinabilityToHumanReadablePhrase(summary['canJoin'])}</div>
+        <div>Players:</div>
+        <ol>
+        {summary['players'].map(player => {
+            return <li className={"other-player-status"} key={player.username}>{player.username + (player.username === username ? " (YOU): " : ": ") + transformStatusToHumanReadableWord(player.status.description)}</li>
         })}
+        </ol>
     </div>;
 }
